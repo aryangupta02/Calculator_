6537e1e6002decb66fd29e12dc658b5d99f26fb2
Aryan Gupta <99682264+aryangupta02@users.noreply.github.com>
2023-07-12 13:41:43 +0530

    Create cal.java

---
+++ cal.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class Calculator {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the first number: ");
+        int num1 = scanner.nextInt();
+
+        System.out.println("Enter the second number: ");
+        int num2 = scanner.nextInt();
+
+        System.out.println("Choose an operation: ");
+        System.out.println("1. Addition");
+        System.out.println("2. Subtraction");
+        System.out.println("3. Multiplication");
+        System.out.println("4. Division");
+
+        int operation = scanner.nextInt();
+
+        int result = 0;
+
+        switch (operation) {
+            case 1:
+                result = num1 + num2;
+                break;
+            case 2:
+                result = num1 - num2;
+                break;
+            case 3:
+                result = num1 * num2;
+                break;
+            case 4:
+                result = num1 / num2;
+                break;
+            default:
+                System.out.println("Invalid operation");
+                break;
+        }
+
+        System.out.println("The result is: " + result);
+    }
+}
